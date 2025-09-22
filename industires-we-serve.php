22c6464c23b7e09c31a74d9f627b68f685532bf4
supportdcbindia <support@dcbindia.in>
2025-09-22 18:11:02 +0530

    Update industires-we-serve.php

--- industires-we-serve.php
+++ industires-we-serve.php
@@ -8,66 +8,68 @@
 
 
 <div class="space overflow-hidden">
-    <div class="row" id="projectSlide3">
-        <div class="col-md-6 col-lg-4">
-            <div class="project-box">
-                <div class="project-img">
-                    <img src="assets/img/industries-we-serve/beetroot.webp?ver=1.2" alt="Industries We Serve">
-                </div>
-                <div class="project-content">
-                    <h3 class="project-title"><a href="javascript:;">Beetroot</a></h3>
-                    <a href="javascript:;" class="project-icon"><i class="far fa-plus"></i></a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6 col-lg-4">
-            <div class="project-box">
-                <div class="project-img">
-                    <img src="assets/img/industries-we-serve/onion.webp?ver=1.2" alt="Industries We Serve">
-                </div>
-                <div class="project-content">
-                    <h3 class="project-title"><a href="javascript:;">Onion</a></h3>
-                    <a href="javascript:;" class="project-icon"><i class="far fa-plus"></i></a>
+    <div class="container">
+        <div class="row" id="">
+            <div class="col-md-4 mb-2">
+                <div class="project-box">
+                    <div class="project-img">
+                        <img src="assets/img/industries-we-serve/beetroot.webp?ver=1.2" alt="Industries We Serve">
+                    </div>
+                    <div class="project-content">
+                        <h3 class="project-title"><a href="javascript:;">Beetroot</a></h3>
+                        <a href="javascript:;" class="project-icon"><i class="far fa-plus"></i></a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-6 col-lg-4">
-            <div class="project-box">
-                <div class="project-img">
-                    <img src="assets/img/industries-we-serve/potato.webp?ver=1.2"
-                        alt="Industries We Serve">
-                </div>
-                <div class="project-content">
-                    <h3 class="project-title"><a href="javascript:;">Potato</a></h3>
-                    <a href="javascript:;" class="project-icon"><i class="far fa-plus"></i></a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6 col-lg-4">
-            <div class="project-box">
-                <div class="project-img">
-                    <img src="assets/img/industries-we-serve/cabbage.webp?ver=1.2"
-                        alt="Industries We Serve">
-                </div>
-                <div class="project-content">
-                    <h3 class="project-title"><a href="javascript:;">Cabbage</a></h3>
-                    <a href="javascript:;" class="project-icon"><i class="far fa-plus"></i></a>
+            <div class="col-md-4 mb-2">
+                <div class="project-box">
+                    <div class="project-img">
+                        <img src="assets/img/industries-we-serve/onion.webp?ver=1.2" alt="Industries We Serve">
+                    </div>
+                    <div class="project-content">
+                        <h3 class="project-title"><a href="javascript:;">Onion</a></h3>
+                        <a href="javascript:;" class="project-icon"><i class="far fa-plus"></i></a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-6 col-lg-4">
-            <div class="project-box">
-                <div class="project-img">
-                    <img src="assets/img/industries-we-serve/carrot.webp?ver=1.2"
-                        alt="Industries We Serve">
-                </div>
-                <div class="project-content">
-                    <h3 class="project-title"><a href="javascript:;">Carrot</a></h3>
-                    <a href="javascript:;" class="project-icon"><i class="far fa-plus"></i></a>
+            <div class="col-md-4 mb-2">
+                <div class="project-box">
+                    <div class="project-img">
+                        <img src="assets/img/industries-we-serve/potato.webp?ver=1.2"
+                            alt="Industries We Serve">
+                    </div>
+                    <div class="project-content">
+                        <h3 class="project-title"><a href="javascript:;">Potato</a></h3>
+                        <a href="javascript:;" class="project-icon"><i class="far fa-plus"></i></a>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div class="col-md-4 mb-2">
+                <div class="project-box">
+                    <div class="project-img">
+                        <img src="assets/img/industries-we-serve/cabbage.webp?ver=1.2"
+                            alt="Industries We Serve">
+                    </div>
+                    <div class="project-content">
+                        <h3 class="project-title"><a href="javascript:;">Cabbage</a></h3>
+                        <a href="javascript:;" class="project-icon"><i class="far fa-plus"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-2">
+                <div class="project-box">
+                    <div class="project-img">
+                        <img src="assets/img/industries-we-serve/carrot.webp?ver=1.2"
+                            alt="Industries We Serve">
+                    </div>
+                    <div class="project-content">
+                        <h3 class="project-title"><a href="javascript:;">Carrot</a></h3>
+                        <a href="javascript:;" class="project-icon"><i class="far fa-plus"></i></a>
+                    </div>
+                </div>
+            </div>
 
+        </div>
     </div>
 </div>
 <?php include('footer.php')  ?>
